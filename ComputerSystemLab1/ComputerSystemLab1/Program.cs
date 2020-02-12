59f1c75fdcd005e99f14d85b709f95c64f9460dc
Ваня Хоменко <wanya.xomenko@gmail.com>
2020-02-12 09:27:47 +0200

    first commit

---
+++ ComputerSystemLab1/ComputerSystemLab1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+namespace ComputerSystemLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            Console.WriteLine("Ця програма рахує відносну частоту появи символу в тексті.");
+            Console.WriteLine("В тексті було знайдено:");
+            Console.WriteLine("#################################################");
+            System.IO.StreamReader sr = System.IO.File.OpenText(@"C:\Users\wanya\OneDrive\Робочий стіл\СS_LAB1\text_3_1.txt.bz2");
+            string stringData = sr.ReadToEnd();
+            sr.Close();
+            Parsesthefile(stringData, b);
+            Console.ReadKey();
+        }
+        public static void Parsesthefile(string line, int a)
+        {
+            bool q = true;
+            double probabil = 0; //імовірність
+            double summ = 0; // всі символи
+            double entroph = 0;// ентропія
+            double b = 0; 
+            double HINT = 0;
+            char[] line2 = line.ToCharArray();
+            // string symvols = "АаБбВвГгҐґДдЕеЄєЖжЗзИиІіЇїЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЬьЮюЯя .,";
+            string symvols = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm +., / 1234567890";
+
+            char[] symvols2 = symvols.ToCharArray();
+            using (var reader = new StreamReader(@"C:\Users\wanya\OneDrive\Робочий стіл\СS_LAB1\text_3_1.txt.bz2", detectEncodingFromByteOrderMarks: true))
+            {
+                while (reader.Read() > -1)
+                {
+                    summ++;
+                }
+            }
+            
+            for (int i = 0; i < 34; i++)
+            {
+                for (int j = 0; j < line.Length; j++)
+
+                    if ((symvols2[2 * i] == line2[j]) || (symvols2[2 * i + 1] == line2[j]))
+                    {
+                        a = a + 1;
+                    };
+                probabil = a / summ;
+                entroph = probabil * Math.Log2(1 / probabil);
+                if (entroph > 0)
+                    b = b + (probabil * Math.Log2(1.0 / probabil));
+                if (a == 0)
+                {
+                    entroph = 0;
+                }
+                if (a != 0)
+
+                    if (q) Console.WriteLine("");
+                {
+                    Console.WriteLine("Cимвол {0:d} зустрічається {1:d} раз з ймовірністю {2:f8}. Ентропія символа = {3:f8}", symvols2[2 * i], a, probabil, entroph);
+                    q = false;
+                }
+                a = 0;
+            }
+            HINT = summ * b;
+            Console.WriteLine("Загальна ентропія: {0:f8}", b);
+            Console.WriteLine("HINT файлу: {0:f8}", HINT);
+        }
+    }
+}
---
+++ ComputerSystemlLab1_2/ComputerSystemlLab1_2/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+
+namespace ComputerSystemlLab1_2
+{
+    class Program
+    {
+        static readonly string textFile = @"C:\Users\wanya\OneDrive\Робочий стіл\СS_LAB1\text_3.txt";
+        static void Main(string[] args)
+        {
+			var text = "";
+			if (File.Exists(textFile))
+			{
+				text = File.ReadAllText(textFile);
+			}
+			var str = "";
+			str = text;
+			Console.WriteLine("Текст, що міститься в файлі: ");
+			Console.WriteLine(str);
+			Console.WriteLine();
+			Console.WriteLine("Довжина тексту: ");
+			Console.WriteLine(str.Length);
+			byte[] data = System.Text.Encoding.Default.GetBytes(str);
+			char[] value = Base64(data);
+			Console.WriteLine("\nЗакодований текст: ");
+			Console.WriteLine(value);
+			Console.WriteLine("\nДовжина закодованого тексту: ");
+			Console.WriteLine(value.Length);
+		}
+		public static char[] Base64(byte[] data)
+		{
+			int leng, leng2;
+			int bCount;
+			int padCount;
+
+			leng = data.Length;
+
+			if ((leng % 3) == 0)
+			{
+				padCount = 0;
+				bCount = leng / 3;
+			}
+			else
+			{
+				padCount = 3 - (leng % 3);
+				bCount = (leng + padCount) / 3;
+			}
+
+			leng2 = leng + padCount;
+
+			byte[] source2;
+			source2 = new byte[leng2];
+
+			for (int x = 0; x < leng2; x++)
+			{
+				if (x < leng)
+				{
+					source2[x] = data[x];
+				}
+				else
+				{
+					source2[x] = 0;
+				}
+			}
+
+			byte b1, b2, b3;
+			byte temp, temp1, temp2, temp3, temp4;
+			byte[] buffer = new byte[bCount * 4];
+			char[] result = new char[bCount * 4];
+
+			for (int x = 0; x < bCount; x++)
+			{
+				b1 = source2[x * 3];
+				b2 = source2[x * 3 + 1];
+				b3 = source2[x * 3 + 2];
+
+				temp1 = (byte)((b1 & 252) >> 2);
+
+				temp = (byte)((b1 & 3) << 4);
+				temp2 = (byte)((b2 & 240) >> 4);
+				temp2 += temp;
+
+				temp = (byte)((b2 & 15) << 2);
+				temp3 = (byte)((b3 & 192) >> 6);
+				temp3 += temp;
+
+				temp4 = (byte)(b3 & 63);
+
+				buffer[x * 4] = temp1;
+				buffer[x * 4 + 1] = temp2;
+				buffer[x * 4 + 2] = temp3;
+				buffer[x * 4 + 3] = temp4;
+
+			}
+
+			for (int x = 0; x < bCount * 4; x++)
+			{
+				result[x] = BitToChar(buffer[x]);
+			}
+
+			switch (padCount)
+			{
+				case 0:
+					break;
+				case 1:
+					result[bCount * 4 - 1] = '=';
+					break;
+				case 2:
+					result[bCount * 4 - 1] = '=';
+					result[bCount * 4 - 2] = '=';
+					break;
+				default:
+					break;
+			}
+
+			return result;
+		}
+
+		private static char BitToChar(byte b)
+		{
+			char[] lookupTable = new char[64] {
+		'A','B','C','D','E','F','G','H','I','J','K','L','M',
+		'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+		'a','b','c','d','e','f','g','h','i','j','k','l','m',
+		'n','o','p','q','r','s','t','u','v','w','x','y','z',
+		'0','1','2','3','4','5','6','7','8','9','+','/'
+	};
+
+			if ((b >= 0) && (b <= 63))
+			{
+				return lookupTable[(int)b];
+			}
+			else
+			{
+				return ' ';
+			}
+		}
+	
+}
+}
